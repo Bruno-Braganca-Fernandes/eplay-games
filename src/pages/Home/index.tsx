7d853ff4432a2805d33dbeb1b1998535c0bf138a
bruno <bruno.b.fernandes12@hotmail.com>
2025-07-29 20:26:53 -0300

    feat: refactor game data handling and update components to fetch data from API
    - Format the currency with the intl object
    
    - Develop the games category section.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,104 +1,63 @@
+import { useEffect, useState } from 'react'
+
 import Banner from '../../components/Banner'
 import ProductList from '../../components/ProductsList'
-import Game from '../../models/Game'
 
 import resident from '../../assets/images/resident.png'
 import diablo from '../../assets/images/diablo.png'
 import zelda from '../../assets/images/zelda.png'
 import starWars from '../../assets/images/star_wars.png'
 
-const promocoes: Game[] = [
-  {
-    id: 1,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror',
-    image: resident,
-    infos: ['10%', 'R$ 250,00'],
-    system: 'Windows',
-    title: 'Resident Evil 4'
-  },
-  {
-    id: 2,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror',
-    image: resident,
-    infos: ['5%', 'R$ 290,00'],
-    system: 'PS5',
-    title: 'Resident Evil 4'
-  },
-  {
-    id: 3,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror',
-    image: resident,
-    infos: ['10%', 'R$ 250,00'],
-    system: 'PC, PS5',
-    title: 'Resident Evil 4'
-  },
-  {
-    id: 4,
-    category: 'Aventura',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror',
-    image: resident,
-    infos: ['10%', 'R$ 250,00'],
-    system: 'PC, Xbox',
-    title: 'Resident Evil 4'
-  }
-]
+export interface GalleryItem {
+  type: 'image' | 'video'
+  url: string
+}
 
-const emBreve: Game[] = [
-  {
-    id: 5,
-    category: 'RPG',
-    description:
-      'Diablo IV é um jogo eletrônico de RPG de ação desenvolvido e publicado pela Blizzard Entertainment.',
-    image: diablo,
-    infos: ['17/05'],
-    system: 'Windows',
-    title: 'Diablo IV'
-  },
-  {
-    id: 6,
-    category: 'RPG',
-    description:
-      'Diablo IV é um jogo eletrônico de RPG de ação desenvolvido e publicado pela Blizzard Entertainment.',
-    image: zelda,
-    infos: ['17/05'],
-    system: 'Windows',
-    title: 'Zelda'
-  },
-  {
-    id: 7,
-    category: 'RPG',
-    description:
-      'Diablo IV é um jogo eletrônico de RPG de ação desenvolvido e publicado pela Blizzard Entertainment.',
-    image: starWars,
-    infos: ['17/05'],
-    system: 'Windows',
-    title: 'Star Wars'
-  },
-  {
-    id: 8,
-    category: 'RPG',
-    description:
-      'Diablo IV é um jogo eletrônico de RPG de ação desenvolvido e publicado pela Blizzard Entertainment.',
-    image: resident,
-    infos: ['17/05'],
-    system: 'Nintendo Switch',
-    title: 'Resident Evil 4'
+export type Game = {
+  id: number
+  name: string
+  description: string
+  release_date?: string
+  prices: {
+    discount?: number
+    old?: number
+    current?: number
   }
-]
+  details: {
+    category: string
+    system: string
+    developer: string
+    publisher: string
+    language: string[]
+  }
+  media: {
+    thumbnail: string
+    cover: string
+    gallery: GalleryItem[]
+  }
+}
 
-const Home = () => (
-  <>
-    <Banner />
-    <ProductList games={promocoes} title="Promoções" background="gray" />
-    <ProductList games={emBreve} title="Em breve" background="black" />
-  </>
-)
+const Home = () => {
+  const [promocoes, setPromocoes] = useState<Game[]>([])
+  const [emBreve, setEmBreve] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/eplay/promocoes')
+      .then((res) => res.json())
+      .then((res) => setPromocoes(res))
+
+    fetch('https://fake-api-tau.vercel.app/api/eplay/em-breve')
+      .then((res) => res.json())
+      .then((res) => setEmBreve(res))
+  }, [])
+
+  return (
+    <>
+      <Banner />
+      <ProductList games={promocoes} title="Promoções" background="gray" />
+      <ProductList games={emBreve} title="Em breve" background="black" />
+    </>
+  )
+}
 
 export default Home
